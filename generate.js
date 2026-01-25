95b0690fe9343d5efd4d2b2de3b18b4ba01173aa
Shashank <shashankg5518@gmail.com>
2026-01-25 19:40:28 +0530

    update media

--- generate.js
+++ generate.js
@@ -1,11 +1,19 @@
 const fs = require("fs");
+const path = require("path");
 
-const folder = "./photos";
+const folder = path.join(__dirname, "photos");
+const out = path.join(__dirname, "media.json");
+
+// delete old file if exists
+if (fs.existsSync(out)) {
+  fs.chmodSync(out, 0o666);
+  fs.unlinkSync(out);
+}
 
 const files = fs.readdirSync(folder).filter(f =>
   /\.(jpe?g|png|gif|webp|mp4|webm|mov)$/i.test(f)
 );
 
-fs.writeFileSync("media.json", JSON.stringify(files, null, 2));
+fs.writeFileSync(out, JSON.stringify(files, null, 2));
 
 console.log("media.json created with", files.length, "files");
